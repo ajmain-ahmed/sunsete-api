b8db7ddcad78b174924da680e600b9970a239940
Noon <gdaftrnoon@gmail.com>
2026-01-01 15:33:52 +0000

    rate limiter, making publicly available

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import json
 from database import supabase
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from upstash_redis import Redis
 from fastapi.middleware.cors import CORSMiddleware
+from upstash_ratelimit import Ratelimit, FixedWindow
 
 app = FastAPI()
 redis = Redis.from_env()
 
 origins = [
-    "http://localhost:3000",
-    "http://localhost:8080",
+    "*",
 ]
+
+ratelimit = Ratelimit(
+    redis=Redis.from_env(),
+    limiter=FixedWindow(max_requests=100, window=60),
+    prefix="@upstash/ratelimit",
+)
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,16 +29,25 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Welcome": "to sunsete-api"}
 
 @app.get("/v1/define/{word}")
-def define(word: str):
+def define(word: str, request: Request):
+
     cache_key = word
-    
+    client_host = request.client.host or 'unknown'
+    response = ratelimit.limit(client_host)
+
+    if not response.allowed:
+        return {'source':f'{client_host}', 'query': word, 'data':'rate limited', 'error': True}
+
     cached_data =  redis.get(cache_key)
     if cached_data:
-        return {'source': 'cache', 'query': word, 'data': json.loads(cached_data)}
+        return {'source': 'cache', 'query': word, 'data': json.loads(cached_data), 'error': False}
 
     data = supabase.rpc('def', params={'word': word}).execute()
-    redis.setex(cache_key, 21600, data.data)
-    return {'source':'api', 'query': word, 'data':data.data}
+    if data.data is not None:
+        redis.setex(cache_key, 21600, data.data)
+        return {'source':'api', 'query': word, 'data':data.data, 'error': False}
+    else:
+        return {'source':'api', 'query': word, 'data':None, 'error': True}
